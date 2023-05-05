8e51831e5a421a9659f3159ff8fe4ca415bf539e
Fl0k3n <flok3n@gmail.com>
2023-05-05 20:47:13 +0200

    basic wave management

---
+++ Assets/Scripts/Waves/SpawnInfo.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnInfo
+{
+    public string enemyType;
+    public float spawnCooldown;
+    public int health;
+    public float speed;
+    public Dictionary<string, string> extraParams;
+}
---
+++ Assets/Scripts/Waves/WaveManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+
+
+public class WaveManager : Singleton<WaveManager>
+{
+    private int wave = 0;
+    private List<Wave> currentLevelWaves;
+
+    [SerializeField]
+    private Text waveTxt;
+
+    List<EnemyShip> activeEnemies = new List<EnemyShip>();
+
+    [SerializeField]
+    private GameObject waveBtn;
+
+    private Wave CurrentWave { get => currentLevelWaves[wave - 1]; }
+
+    public bool WaveActive
+    {
+        get
+        {
+            return activeEnemies.Count > 0;
+        }
+    }
+
+    public void StartWave()
+    {
+        wave++;
+        waveTxt.text = string.Format("Wave: <color=lime>{0}</color>", wave);
+        StartCoroutine(SpawnWave());
+        waveBtn.SetActive(false);
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        LevelManager.Instance.GeneratePath();
+
+        foreach(SpawnInfo spawnInfo in CurrentWave.spawnInfos)
+        {
+            EnemyShip enemy = GameManager.Instance.Pool.GetObject(spawnInfo.enemyType).GetComponent<EnemyShip>();
+            enemy.Spawn(spawnInfo.health, spawnInfo.speed);
+            activeEnemies.Add(enemy);
+
+            yield return new WaitForSeconds(spawnInfo.spawnCooldown);
+        }
+    }
+
+    public void RemoveEnemy(EnemyShip enemy)
+    {
+        activeEnemies.Remove(enemy);
+        if (!WaveActive && GameManager.Instance.canShowNewWaveButton())
+        {
+            GameManager.Instance.Currency += CurrentWave.waveReward;
+
+            if (wave >= currentLevelWaves.Count)
+            {
+                // GameManager.onLevelFinished(); TODO
+                Debug.Log("Level Finished");
+            } else
+            {
+                waveBtn.SetActive(true);
+            }
+        }
+    }
+
+    public void LoadLevel(string wavesFileName)
+    {
+        TextAsset wavesJson = Resources.Load<TextAsset>(wavesFileName);
+        currentLevelWaves = JsonConvert.DeserializeObject<List<Wave>>(wavesJson.ToString());
+        waveBtn.SetActive(true);
+    }
+}
